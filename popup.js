7ac361ec7085ac3fd58a28358c3a727c3ebb9162
unknown <fcalvo33@gmail.com>
2024-03-17 14:15:26 -0300

    Safe Search V. 1.0

--- popup.js
+++ popup.js
@@ -1,25 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("toggleButton");
+  const statusText = document.getElementById("adsStatus");
 
   chrome.storage.sync.get(["adsEnabled"], function (result) {
-    btn.textContent = result.adsEnabled
-      ? "Mostrar patrocinados"
-      : "Ocultar patrocinados";
+    if (result.hasOwnProperty("adsEnabled")) {
+      btn.checked = result.adsEnabled;
+      statusText.textContent = result.adsEnabled ? "Hidden ads" : "Visible ads";
+    } else {
+      btn.checked = false;
+      statusText.textContent = "Hidden ads";
+    }
   });
 
-  btn.addEventListener("click", () => {
-    chrome.storage.sync.get(["adsEnabled"], function (result) {
-      const newState = !result.adsEnabled;
-      chrome.storage.sync.set({ adsEnabled: newState }, function () {
-        btn.textContent = newState
-          ? "Mostrar patrocinados"
-          : "Ocultar patrocinados";
-        chrome.tabs.query(
-          { active: true, currentWindow: true },
-          function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { adsEnabled: newState });
-          }
-        );
+  btn.addEventListener("change", () => {
+    const newState = btn.checked;
+    chrome.storage.sync.set({ adsEnabled: newState }, function () {
+      statusText.textContent = newState ? "Hidden ads" : "Visible ads";
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { adsEnabled: newState });
       });
     });
   });
